f3c23106c9a88b6d0371713a4e2a34b0639dcbbe
EdgarsBe <edgarsberzins100@gmail.com>
2022-11-19 20:22:09 +0200

    Made a Xamarin.Forms Project

---
+++ Re-Books_App/Re-Books_App/Re-Books_App/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Re_Books_App.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Re-Books_App/Re-Books_App/Re-Books_App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Re_Books_App.Models;
+using Re_Books_App.ViewModels;
+
+namespace Re_Books_App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
